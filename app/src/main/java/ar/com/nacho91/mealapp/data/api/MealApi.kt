72c972270df673f6988fa5177932924717fbbdbb
Ignacio Oviedo <ignaciooviedo@WKSAR147L.fibertel.com.ar>
2019-12-05 08:00:19 -0300

    Add RxJava dependency and update MealApi response

--- app/src/main/java/ar/com/nacho91/mealapp/data/api/MealApi.kt
+++ app/src/main/java/ar/com/nacho91/mealapp/data/api/MealApi.kt
@@ -1,12 +1,14 @@
 package ar.com.nacho91.mealapp.data.api
 
+import ar.com.nacho91.mealapp.data.api.model.MealsResponse
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MealApi {
 
     @GET("search.php")
-    fun searchByName(@Query("s") query: String)
+    fun searchByName(@Query("s") query: String): Single<MealsResponse>
 
     @GET("categories.php")
     fun categories()
