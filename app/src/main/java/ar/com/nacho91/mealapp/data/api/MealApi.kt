812342fe8491432b5e3e8c9218d6ce4404073a32
Ignacio Oviedo <ignaciooviedo@WKSAR147L.fibertel.com.ar>
2019-12-05 07:54:24 -0300

    Add retrofit dependency and update MealApi

--- app/src/main/java/ar/com/nacho91/mealapp/data/api/MealApi.kt
+++ app/src/main/java/ar/com/nacho91/mealapp/data/api/MealApi.kt
@@ -1,8 +1,13 @@
 package ar.com.nacho91.mealapp.data.api
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MealApi {
 
-    fun searchByName(query: String)
+    @GET("search.php")
+    fun searchByName(@Query("s") query: String)
 
+    @GET("categories.php")
     fun categories()
 }
