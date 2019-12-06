b73a167aefcb9b41fd40e28bb2f6124899e15873
Ignacio Oviedo <ignaciooviedo@WKSAR147L.local>
2019-12-06 10:47:49 -0300

    wip

--- app/src/main/java/ar/com/nacho91/mealapp/ui/MealActivity.kt
+++ app/src/main/java/ar/com/nacho91/mealapp/ui/MealActivity.kt
@@ -6,7 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ar.com.nacho91.mealapp.R
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_meal.view.*
 import javax.inject.Inject
 
 class MealActivity : AppCompatActivity() {
@@ -14,15 +18,20 @@ class MealActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private val mealList by lazy { findViewById<RecyclerView>(R.id.meal_list) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meal)
+
+        mealList.layoutManager = LinearLayoutManager(this)
 
         val model = ViewModelProviders.of(this, viewModelFactory)[MealViewModel::class.java]
 
         model.response.observe(this, Observer {
             when (it) {
-                is MealViewModel.MealsResponse.Success -> MealAdapter(it.meals)
+                is MealViewModel.MealsResponse.Success -> mealList.adapter = MealAdapter(it.meals)
                 is MealViewModel.MealsResponse.EmptyResult -> Toast.makeText(this, "No hay resultados", Toast.LENGTH_SHORT).show()
                 is MealViewModel.MealsResponse.Loading -> Toast.makeText(this, "Cargando..", Toast.LENGTH_SHORT).show()
                 is MealViewModel.MealsResponse.Error -> Toast.makeText(this, "¡Ops! Ocurrio un error", Toast.LENGTH_SHORT).show()
